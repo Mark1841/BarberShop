394b8d5b0fbde5038671bce031553d497364e913
Mark1841 <mjanovitz@outlook.com>
2025-06-04 11:51:37 -0400

    Updated add appointment method

--- barberShop.py
+++ barberShop.py
@@ -36,27 +36,30 @@ class BarberShop:
                return employee
         return None
 
-    def add_customer(self):
+    def add_customer(self, name = None):
         """ Adds a customer to the shop """
         customer_id = len(self.customers) + 1
-        name = input('Enter customer name: ')
+        if name is None:
+            name = input('Enter customer name: ')
         telephone_number = input('Enter customer phone number: ')
         email = input('Enter customer email: ')
         new_customer = Customer(customer_id, name, telephone_number, email)
         self.customers.append(new_customer)
 
-    def add_employee(self):
+    def add_employee(self, name = None):
         """ Adds an employee to the shop """
         employee_id = len(self.employees) + 1
-        name = input('Enter employee name: ')
+        if name is None:
+            name = input('Enter employee name: ')
         telephone_number = input('Enter employee phone number: ')
         new_employee = Employee(employee_id, name, telephone_number, True)
         self.employees.append(new_employee)
 
-    def add_service(self):
+    def add_service(self, name = None):
         """ Adds a service to the shop """
         service_id = len(self.services) + 1
-        name = input('Enter service name: ')
+        if name is None:
+            name = input('Enter service name: ')
         description = input('Enter service description: ')
         price = input('Enter service price: ')
         duration = input('Enter service duration: ')
@@ -65,10 +68,25 @@ class BarberShop:
 
     def add_appointment(self):
         """ Adds an appointment to the shop """
-        customer = self.find_customer(input('Enter customers name: '))
-        service = self.find_service(input('Enter the service the customer wants: '))
-        employee = self.find_employee(input('Enter the name of the employee competing the service: '))
+        customer_name = input('Enter customers name: ')
+        customer = self.find_customer(customer_name)
+        if customer is None:
+            print('Customer not found, they will need to be added to database!')
+            self.add_customer(customer_name)
+            customer = self.find_customer(customer_name)
+        service_name = input('Enter the service the customer wants: ')
+        service = self.find_service(service_name)
+        if service is None:
+            print('Service not found, it will need to be added to database!')
+            self.add_service(service_name)
+            service = self.find_service(service_name)
+        employee_name = input('Enter the name of the employee completing the service: ')
+        employee = self.find_employee(employee_name)
+        if employee is None:
+            print('Employee not found, they will need to be added to database!')
+            self.add_employee(employee_name)
+            employee = self.find_employee(employee_name)
         date = input('Enter the appointment date: ')
         status = "Booked"
         new_appointment = Appointment(customer, service, employee, date, status)
-        self.appointments.append(new_appointment)
+        (self.appointments.append(new_appointment))
