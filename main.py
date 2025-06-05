fd71cb00380af9399eae6dc513f36e625d962689
Mark1841 <mjanovitz@outlook.com>
2025-06-05 11:35:09 -0400

    Update to GUI

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 
 from barberShop import BarberShop
 from dataHandler import save_customers, save_employees
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QLabel, QLineEdit
 
 
 class MainWindow(QMainWindow):
@@ -17,17 +17,42 @@ class MainWindow(QMainWindow):
         button_book_appointment = QPushButton("Book Appointment")
         button_quit = QPushButton("Quit")
         label_customer_name = QLabel("Customer Name: ")
+        line_edit_customer_name = QLineEdit()
+        label_customer_telephone = QLabel("Customer Telephone #: ")
+        line_edit_customer_telephone = QLineEdit()
+        label_customer_email = QLabel("Customer Email: ")
+        line_edit_customer_email = QLineEdit()
 
-        layout_vertical = QVBoxLayout()
-        layout_horizontal = QHBoxLayout()
-        layout_vertical.addWidget(button_add_customer)
-        layout_vertical.addWidget(button_add_employee)
-        layout_vertical.addWidget(button_add_service)
-        layout_vertical.addWidget(button_book_appointment)
-        layout_vertical.addWidget(button_quit)
+        layout_vertical1 = QVBoxLayout()
+        layout_vertical2 = QVBoxLayout()
+        layout_horizontal1 = QHBoxLayout()
+        layout_horizontal2 = QHBoxLayout()
+        layout_horizontal3 = QHBoxLayout()
+        layout_horizontal4 = QHBoxLayout()
+
+        layout_vertical1.addWidget(button_add_customer)
+        layout_vertical1.addWidget(button_add_employee)
+        layout_vertical1.addWidget(button_add_service)
+        layout_vertical1.addWidget(button_book_appointment)
+        layout_vertical1.addWidget(button_quit)
+
+        layout_horizontal2.addWidget(label_customer_name)
+        layout_horizontal3.addWidget(label_customer_telephone)
+        layout_horizontal4.addWidget(label_customer_email)
+
+        layout_horizontal2.addWidget(line_edit_customer_name)
+        layout_horizontal3.addWidget(line_edit_customer_telephone)
+        layout_horizontal4.addWidget(line_edit_customer_email)
+
+        layout_horizontal1.addLayout(layout_vertical1)
+        layout_horizontal1.addSpacing(20)
+        layout_vertical2.addLayout(layout_horizontal2)
+        layout_vertical2.addLayout(layout_horizontal3)
+        layout_vertical2.addLayout(layout_horizontal4)
+        layout_horizontal1.addLayout(layout_vertical2)
 
         widget = QWidget()
-        widget.setLayout(layout_vertical)
+        widget.setLayout(layout_horizontal1)
         self.setCentralWidget(widget)
 
         button_quit.clicked.connect(self.close)
